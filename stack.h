dc13e4fedb83f66c566bcfca98cd1f45c2b5b546
Zergey Loshkarev <zeryoshka@gmail.com>
2021-04-30 12:50:28 +0300

    Заголовочный файл для stack

---
+++ stack.h
@@ -0,0 +1,36 @@
+//stack.h
+
+#ifndef STACK_H
+#define STACK_H
+
+
+#include "data_type_dyno_c.h"
+
+
+// Структура для описания узла стека
+typedef struct{
+    struct Node_stack* next;
+    Data_dyno_type data;
+} Node_stack;
+
+// Структура для стека
+typedef struct {
+    Node_stack* start;
+} Stack;
+
+// Инициализация стека
+Stack* init_stack();
+
+// Добавление элемента в стек
+void push(Stack* stack, Data_dyno_type data);
+
+// Удаляет элемент из стека и возвращает его назад
+Data_dyno_type pop(Stack* stack);
+
+// Проверяет элемент стека на пустоту
+int is_empty(Stack* stack);
+
+// Просмотреть верхний элемент
+Data_dyno_type top(Stack* stack);
+
+#endif

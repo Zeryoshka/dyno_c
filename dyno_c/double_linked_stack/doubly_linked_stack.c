8f7b2f844eb47b625fcce2b7bae171217f3698a1
Zergey Loshkarev <zeryoshka@gmail.com>
2021-04-30 18:45:19 +0300

    added .c file for double_linked_list

---
+++ dyno_c/double_linked_stack/doubly_linked_stack.c
@@ -0,0 +1,45 @@
+// doubly_linked_stack.c
+
+#include <stddef.h>
+#include <stdlib.h>
+#include "doubly_linked_stack.h"
+#include "../data_dyno_type.h"
+
+
+// Инициализация двусвязного стека
+Doubly_linked_stack* init_stack(){
+    Doubly_linked_stack* stack = (Doubly_linked_stack*) malloc(sizeof(Doubly_linked_stack));
+    stack->start = NULL;
+    return stack;
+}
+
+// Добавление элемента в двусвязный стек
+void push(Doubly_linked_stack* stack, Data_dyno_type data) {
+    Node_doubly_linked_stack* node = (Node_doubly_linked_stack*) malloc(sizeof(Node_doubly_linked_stack));
+    node->data = data;
+    node->next = stack->start;
+    node->prev = NULL;
+    if (stack->start != NULL)
+        stack->start->prev = node;
+    stack->start = node;
+}
+
+// Удаляет элемент из двусвязного стека и возвращает его назад
+Data_dyno_type pop(Doubly_linked_stack* stack) {
+    Data_dyno_type data = stack->start->data;
+    Node_doubly_linked_stack* new_top = stack->start->next;
+    new_top->prev = NULL;
+    free(stack->start);
+    stack->start = new_top;
+    return data;
+}
+
+// Проверяет двусвязный стек на пустоту
+int is_empty(Doubly_linked_stack* stack) {
+    return stack->start == NULL;
+}
+
+// Просмотреть верхний элемент двусвязного стека
+Data_dyno_type top(Doubly_linked_stack* stack){
+    return stack->start->data;
+}
